f2b66d38525d27a67240fb6fbd4da29271b2a5ef
Alexander <alexander@anak.dk>
2021-08-19 11:44:27 +0200

    small fixes for a better working encoder

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 
-//Encoder FRenc(2, 3);
+Encoder FRenc(2, 3);
 //Encoder FLenc(4, 5);
 Encoder BRenc(0, 1);
 //Encoder BLenc(6, 7);
@@ -17,7 +17,7 @@ uint32_t mCurPosValue = 0;
 uint8_t FRpwmA = 10;
 uint8_t FRpwmB = 11;
 
-uint16_t pulsePerRevolution = 932.8; //11*2*2*21.3=932.8 for 2 encoders with 11 pulses. quadrature means times another 2 and gear reduction is 21.3
+double pulsePerRevolution = 932.8; //11*2*2*21.3=932.8 for 2 encoders with 11 pulses. quadrature means times another 2 and gear reduction is 21.3
 
 void countSpeed()
 {
@@ -35,14 +35,14 @@ void setup()
 }
 
 long oldPosition = -999;
-long newPosition = 0;
+//long newPosition = 0;
 
 void getSpeed(Encoder encNumber)
 {
-    newPosition = encNumber.read();
 
-    if (millis() - enc_timer > 50)
+    if (millis() - enc_timer > 1000)
     {
+        long newPosition = encNumber.read();
         if (newPosition != oldPosition)
         {
 
@@ -55,13 +55,13 @@ void getSpeed(Encoder encNumber)
                 FRenc_dir = -1;
             }
             Serial.println(newPosition);
-            //Serial.println(newPosition / 0.05 * 60.0 / pulsePerRevolution * 20.42035225);
+            //Serial.println((double)newPosition / 0.05 * 60.0 / pulsePerRevolution * 20.42035225);
             oldPosition = newPosition;
 
             //Serial.println(FRenc_dir);
-            //encNumber.write(0);
+            FRenc.write(0);
         }
-        encNumber.write(0);
+        //encNumber.write(0);
         enc_timer = millis();
     }
 }
@@ -69,14 +69,14 @@ void getSpeed(Encoder encNumber)
 void loop()
 {
 
-    //getSpeed(FRenc);
+    getSpeed(FRenc);
     //FRenc.write(0);
     //getSpeed(FLenc);
-    getSpeed(BRenc);
+    //getSpeed(BRenc);
     //BRenc.write(0);
     //getSpeed(BLenc);
     //Serial.println(newPosition);
-    //delay(10);
-    //analogWrite(8, 200);
-    analogWrite(9, 0);
+    delay(5);
+    analogWrite(11, 200);
+    analogWrite(10, 0);
 }
